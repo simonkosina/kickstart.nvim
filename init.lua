34e228d91ce316443a896a5513411d59f7b56869
Simon Kosina <skosina@redhat.com>
2025-03-03 11:13:30 +0100

    theme and transparency

--- init.lua
+++ init.lua
@@ -813,6 +813,7 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'rose-pine/neovim',
+    -- 'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
@@ -990,3 +991,5 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+require 'custom.transparency'
