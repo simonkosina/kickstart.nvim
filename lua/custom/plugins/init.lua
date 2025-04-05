592e59476b7eded17fd02774b7b2cfbefe447048
skosina <skosina@redhat.com>
2025-04-05 07:45:43 +0200

    remove tpipeline

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,4 +5,29 @@
 
 ---@module 'lazy'
 ---@type LazySpec
-return {}
+return {
+  {
+    'folke/noice.nvim',
+    event = 'VeryLazy',
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      'MunifTanjim/nui.nvim',
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      'rcarriga/nvim-notify',
+    },
+  },
+  -- {
+  --   'vimpostor/vim-tpipeline',
+  --   config = function()
+  --     -- Automatically embed Vim statusline into tmux
+  --     vim.g.tpipeline_autoembed = 1
+  --     vim.g.tpipeline_restore = 1
+  --     vim.g.tpipeline_clearstl = 1
+  --   end,
+  -- },
+}
