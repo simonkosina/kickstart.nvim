c5d10419694d5f20a1ca5278f13d5fade390cb41
Simon Kosina <skosina@redhat.com>
2025-07-22 11:15:35 +0200

    ts comments plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,6 +7,12 @@
 ---@type LazySpec
 return {
   {
+    'folke/ts-comments.nvim',
+    opts = {},
+    event = 'VeryLazy',
+    enabled = vim.fn.has 'nvim-0.10.0' == 1,
+  },
+  {
     'folke/noice.nvim',
     event = 'VeryLazy',
     opts = {
