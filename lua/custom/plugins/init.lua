9ad48a68bb88be4ffb81935612d39f471b57f3c2
skosina <skosina@redhat.com>
2025-08-30 14:16:29 +0200

    autopairs

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -27,13 +27,4 @@ return {
       'rcarriga/nvim-notify',
     },
   },
-  -- {
-  --   'vimpostor/vim-tpipeline',
-  --   config = function()
-  --     -- Automatically embed Vim statusline into tmux
-  --     vim.g.tpipeline_autoembed = 1
-  --     vim.g.tpipeline_restore = 1
-  --     vim.g.tpipeline_clearstl = 1
-  --   end,
-  -- },
 }
