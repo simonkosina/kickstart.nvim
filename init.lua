0a4bfc13da7493ebeba873723c62d454e92d2dee
Simon Kosina <skosina@redhat.com>
2026-02-06 11:54:07 +0100

    default theme

--- init.lua
+++ init.lua
@@ -410,17 +410,20 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>sF', function()
-        builtin.find_files { hidden = true, no_ignore = true }
-      end, { desc = '[S]earch [F]iles (hidden/ignored)' })
+      vim.keymap.set('n', '<leader>sF', function() builtin.find_files { hidden = true, no_ignore = true } end, { desc = '[S]earch [F]iles (hidden/ignored)' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set({ 'n', 'v' }, '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sG', function()
-        builtin.live_grep {
-          additional_args = { '--hidden', '--no-ignore' },
-        }
-      end, { desc = '[S]earch by [G]rep (hidden/ignored)' })
+      vim.keymap.set(
+        'n',
+        '<leader>sG',
+        function()
+          builtin.live_grep {
+            additional_args = { '--hidden', '--no-ignore' },
+          }
+        end,
+        { desc = '[S]earch by [G]rep (hidden/ignored)' }
+      )
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
@@ -840,7 +843,7 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'sonokai'
-      vim.cmd.colorscheme 'monokai-pro'
+      vim.cmd.colorscheme 'default'
     end,
   },
 
